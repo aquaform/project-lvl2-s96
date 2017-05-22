a0b65b1814f09792d4d9a2d8db8dfabc7ec72e14
aquaform <aquaform@yandex.ru>
2017-05-22 16:25:17 +0500

    fix errors

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,12 +1,6 @@
 #!/usr/bin/env node
-import program from 'commander';
-import start from '..';
-import pkg from '../../package.json';
 
-program.version(pkg.version)
-  .description('Compares two configuration files and shows a difference.')
-  .arguments('<firstConfig> <secondConfig>')
-  .option('-f, --format [type]', 'output format');
+import { start, program } from '..';
 
 program.parse(process.argv);
 start();
--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-export default () => {
+import commander from 'commander';
+import pkg from '../package.json';
+
+export const start = () => {
   console.log('Welcome!');
 };
+
+export const program = commander;
+program.version(pkg.version)
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<firstConfig> <secondConfig>')
+  .option('-f, --format [type]', 'output format');
