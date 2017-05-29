581a4c25fb7d9df0f8335bf468b8dfe8e84f24b8
aquaform <aquaform@yandex.ru>
2017-05-29 14:55:09 +0500

    fix errors

--- src/strategies/renderAstToTree.js
+++ src/strategies/renderAstToTree.js
@@ -1,28 +1,31 @@
 const indentSize = '  ';
 
 const renderAstToTree = (ast, indents = 1) => {
+  const newIndentSize = indentSize.repeat(indents);
+  const newIndents = indents + 2;
+
   const result = ast.map((node) => {
     switch (node.type) {
       case 'changed':
         if (node.children.length === 0) {
-          return `${indentSize.repeat(indents)}+ ${node.key}: ${node.afterValue}\n${indentSize.repeat(indents)}- ${node.key}: ${node.beforeValue}`;
+          return `${newIndentSize}+ ${node.key}: ${node.afterValue}\n${newIndentSize}- ${node.key}: ${node.beforeValue}`;
         }
-        return `${indentSize.repeat(indents)}+ ${node.key}: ${renderAstToTree(node.children, indents + 2)}`;
+        return `${newIndentSize}+ ${node.key}: ${renderAstToTree(node.children, newIndents)}`;
       case 'unchanged':
         if (node.children.length === 0) {
-          return `${indentSize.repeat(indents)}  ${node.key}: ${node.afterValue}`;
+          return `${newIndentSize}  ${node.key}: ${node.afterValue}`;
         }
-        return `${indentSize.repeat(indents)}  ${node.key}: ${renderAstToTree(node.children, indents + 2)}`;
+        return `${newIndentSize}  ${node.key}: ${renderAstToTree(node.children, newIndents)}`;
       case 'removed':
         if (node.children.length === 0) {
-          return `${indentSize.repeat(indents)}- ${node.key}: ${node.beforeValue}`;
+          return `${newIndentSize}- ${node.key}: ${node.beforeValue}`;
         }
-        return `${indentSize.repeat(indents)}- ${node.key}: ${renderAstToTree(node.children, indents + 2)}`;
+        return `${newIndentSize}- ${node.key}: ${renderAstToTree(node.children, newIndents)}`;
       case 'added':
         if (node.children.length === 0) {
-          return `${indentSize.repeat(indents)}+ ${node.key}: ${node.afterValue}`;
+          return `${newIndentSize}+ ${node.key}: ${node.afterValue}`;
         }
-        return `${indentSize.repeat(indents)}+ ${node.key}: ${renderAstToTree(node.children, indents + 2)}`;
+        return `${newIndentSize}+ ${node.key}: ${renderAstToTree(node.children, newIndents)}`;
       default:
     }
     return null;
