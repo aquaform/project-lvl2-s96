c683e4f7f9b500ef2014ff229fd38b64879c2f73
aquaform <aquaform@yandex.ru>
2017-05-29 14:46:05 +0500

    fix errors

--- src/strategies/renderAstToTree.js
+++ src/strategies/renderAstToTree.js
@@ -1,31 +1,33 @@
-const renderAstToTree = (ast, indents = 2) => {
+const indentSize = '  ';
+
+const renderAstToTree = (ast, indents = 1) => {
   const result = ast.map((node) => {
     switch (node.type) {
       case 'changed':
         if (node.children.length === 0) {
-          return `${' '.repeat(indents)}+ ${node.key}: ${node.afterValue}\n${' '.repeat(indents)}- ${node.key}: ${node.beforeValue}`;
+          return `${indentSize.repeat(indents)}+ ${node.key}: ${node.afterValue}\n${indentSize.repeat(indents)}- ${node.key}: ${node.beforeValue}`;
         }
-        return `${' '.repeat(indents)}+ ${node.key}: ${renderAstToTree(node.children, indents + 4)}`;
+        return `${indentSize.repeat(indents)}+ ${node.key}: ${renderAstToTree(node.children, indents + 2)}`;
       case 'unchanged':
         if (node.children.length === 0) {
-          return `${' '.repeat(indents)}  ${node.key}: ${node.afterValue}`;
+          return `${indentSize.repeat(indents)}  ${node.key}: ${node.afterValue}`;
         }
-        return `${' '.repeat(indents)}  ${node.key}: ${renderAstToTree(node.children, indents + 4)}`;
+        return `${indentSize.repeat(indents)}  ${node.key}: ${renderAstToTree(node.children, indents + 2)}`;
       case 'removed':
         if (node.children.length === 0) {
-          return `${' '.repeat(indents)}- ${node.key}: ${node.beforeValue}`;
+          return `${indentSize.repeat(indents)}- ${node.key}: ${node.beforeValue}`;
         }
-        return `${' '.repeat(indents)}- ${node.key}: ${renderAstToTree(node.children, indents + 4)}`;
+        return `${indentSize.repeat(indents)}- ${node.key}: ${renderAstToTree(node.children, indents + 2)}`;
       case 'added':
         if (node.children.length === 0) {
-          return `${' '.repeat(indents)}+ ${node.key}: ${node.afterValue}`;
+          return `${indentSize.repeat(indents)}+ ${node.key}: ${node.afterValue}`;
         }
-        return `${' '.repeat(indents)}+ ${node.key}: ${renderAstToTree(node.children, indents + 4)}`;
+        return `${indentSize.repeat(indents)}+ ${node.key}: ${renderAstToTree(node.children, indents + 2)}`;
       default:
     }
     return null;
   });
-  return `{\n${result.join('\n')}\n${' '.repeat(indents - 2)}}`;
+  return `{\n${result.join('\n')}\n${indentSize.repeat(indents - 1)}}`;
 };
 
 export default renderAstToTree;
